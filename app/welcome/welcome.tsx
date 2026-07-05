a6906a92a8855a986c96ea6bb76aafea4ead5333
erujs <a.m.jerus@gmail.com>
2026-07-06 03:07:20 +0800

    refactor: app version update

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useAPI } from "~/hooks/use-api";
+import SearchBar from "~/components/search-bar";
+import HeaderShowcase from "~/components/header-showcase";
+import { GameSection } from "~/components/game-section";
+import type { Games } from "~/lib/data-types";
+
+export function Welcome() {
+	const [search, setSearch] = useState("");
+	const [showSearch, setShowSearch] = useState(false);
+	const API_ENDPOINT = import.meta.env.VITE_GAMES_API_ENDPOINT || "http://localhost:3000";
+	const { data: games, error, loading } = useAPI<Games[]>(`${API_ENDPOINT}/api/games`);
+
+	// if (error) throw new Error(error)
+
+	return (
+		<>
+			<HeaderShowcase />
+
+			<div className="container mx-auto p-6">
+				{loading && (
+					<div className="fixed top-0 left-0 w-full h-1 z-50">
+						<div className="h-1 w-full bg-cyan-400 animate-pulse" />
+					</div>
+				)}
+
+				{games?.map((group) => (
+					<GameSection
+						key={group.id}
+						group={group}
+						API_ENDPOINT={API_ENDPOINT}
+						search={search}
+						isHome />
+				))}
+
+				<SearchBar
+					search={search}
+					setSearch={setSearch}
+					showSearch={showSearch}
+					setShowSearch={setShowSearch} />
+			</div>
+		</>
+	);
+}
